d008df1b7ff610fa18145287991fb5f6432d0716
ThomasRizzo <tomrizzo@gmail.com>
2025-12-07 13:50:13 -0500

    Remove temp_embassy directory and update .gitignore

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![no_std]
+
+//! PIO SPI library for RP2350
+//!
+//! This library provides SPI communication functionality using the RP2350's
+//! Programmable I/O (PIO) blocks.
+
+pub mod pio_spi {
+    // TODO: Implement PIO SPI functionality
+}
